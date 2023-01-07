1bedb4a085b335366e73d7f7999579cbbe5cec89
BrokenToast <luispatb@gmail.com>
2023-01-07 12:23:43 +0100

    07012023 Reparado error Scrol lateral y añadido .htaccess

--- Pruebas/objetos_del_navegador.js
+++
@@ -1,4 +0,0 @@
-//windows
-// Navigator contiene propiedades que almacena informacion del navegador
-// Screen contiene información sobre la pantalla
-// Location contiene informacion sobre la url y metodos
